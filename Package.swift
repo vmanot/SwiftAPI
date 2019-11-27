b2086d9b27ecacfda67109e47c8c0c36376137a9
Vatsal Manot <vatsal.manot@yahoo.com>
2019-11-27 13:56:36 +0530

    Update package

--- Package.swift
+++ Package.swift
@@ -15,14 +15,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "git@github.com:vmanot/CombineX.git", .branch("master")),
-        .package(url: "git@github.com:vmanot/Concurrency.git", .branch("master")),
-        .package(url: "git@github.com:vmanot/Network.git", .branch("master")),
-        .package(url: "git@github.com:vmanot/Swallow.git", .branch("master"))
+        .package(url: "git@github.com:vmanot/SwiftUIX.git", .branch("master"))
     ],
     targets: [
         .target(
             name: "API",
-            dependencies: ["CombineX", "Concurrency", "Network", "Swallow"],
+            dependencies: ["CombineX", "SwiftUIX"],
             path: "Sources"
         ),
     ],
