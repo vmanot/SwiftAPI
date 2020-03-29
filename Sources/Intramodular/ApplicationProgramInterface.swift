0569c3ff406a0b71d363d7a1c7f7d41e5ff66bd4
Vatsal Manot <vatsal.manot@yahoo.com>
2020-03-30 03:09:41 +0530

    Update package

--- Sources/Intramodular/ApplicationProgramInterface.swift
+++ Sources/Intramodular/ApplicationProgramInterface.swift
@@ -8,14 +8,4 @@ import Swift
 public protocol ApplicationProgramInterface {
     associatedtype Request: API.Request
     associatedtype Failure: Error = Request.Error
-    
-    var name: String { get }
-}
-
-// MARK: - Implementation -
-
-extension ApplicationProgramInterface {
-    public var name: String {
-        "Application Program Interface"
-    }
 }
