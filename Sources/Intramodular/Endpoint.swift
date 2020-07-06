44fb26f8b744015f50dfcd9ee66f5a34fcbbbf1b
Vatsal Manot <vatsal.manot@yahoo.com>
2020-07-07 04:37:20 +0530

    Update package

--- Sources/Intramodular/Endpoint.swift
+++ Sources/Intramodular/Endpoint.swift
@@ -5,14 +5,27 @@
 import Combine
 import Swallow
 
+/// An type that represents an endpoint of some API.
 public protocol Endpoint {
+    /// The API this endpoint is associated to.
     associatedtype Root: ProgramInterface
     
+    /// The endpoint's input.
     associatedtype Input: Encodable
+    
+    /// The endpoint's output.
     associatedtype Output: Decodable
     
-    func buildRequest(for _: Root, from _: Input) throws -> Root.Request
-    func decodeOutput(from _: Root.Request.Response) throws -> Output
+    /// Build a request.
+    ///
+    /// - Parameter root: The API that this endpoint belongs to.
+    /// - Parameter input: The input required to construct a request for the endpoint.
+    func buildRequest(for root: Root, from input: Input) throws -> Root.Request
+    
+    /// Decode output.
+    ///
+    /// - Parameter response: The request response to decode into the endpoint's output.
+    func decodeOutput(from response: Root.Request.Response) throws -> Output
 }
 
 // MARK: - Auxiliary Implementation -
