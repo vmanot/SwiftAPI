8971a1d4e5881b954127f50288cfc51372d36a7c
Vatsal Manot <vatsal.manot@yahoo.com>
2024-07-29 00:10:08 -0700

    Update package

--- Sources/Intramodular/Error Handling/APIErrorProtocol.swift
+++ Sources/Intramodular/Error Handling/APIErrorProtocol.swift
@@ -14,12 +14,21 @@ public protocol APIErrorProtocol: _ErrorX {
     associatedtype API: APISpecification
     
     static func badRequest(
-        _ error: API.Request.Error
+        request: API.Request?,
+        error: API.Request.Error
     ) -> Self
     
     static func runtime(
         _ error: AnyError
     ) -> Self
+}
+
+extension APIErrorProtocol {
+    public static func badRequest(
+        _ error: API.Request.Error
+    ) -> Self {
+        self.badRequest(request: nil, error: error)
+    }
 }
 
 // MARK: - Initializers
@@ -41,16 +50,20 @@ extension APIErrorProtocol {
 // MARK: - Implemented Conformances
 
 public enum _DefaultAPIError<API: APISpecification>: APIErrorProtocol {
-    case badRequest(API.Request.Error)
+    case badRequest(request: API.Request?, error: API.Request.Error)
     case runtime(AnyError)
     
     public var description: String {
         switch self {
-            case .badRequest(let error):
+            case .badRequest(let request, let error):
                 if let error = ((error as? AnyError)?.base ?? error) as? LocalizedError {
                     return error.localizedDescription
                 } else {
-                    return "Bad request: \(error)"
+                    if let request {
+                        return "Bad request: \(request), error: \(error)"
+                    } else {
+                        return "Bad request: \(error)"
+                    }
                 }
             case .runtime(let error):
                 return error.description
@@ -61,7 +74,7 @@ public enum _DefaultAPIError<API: APISpecification>: APIErrorProtocol {
 extension _DefaultAPIError: LocalizedError {
     public var errorDescription: String? {
         switch self {
-            case .badRequest(let error):
+            case .badRequest(_, let error):
                 return (error as? LocalizedError)?.localizedDescription ?? error.localizedDescription
             case .runtime(let error):
                 return (error as? LocalizedError)?.localizedDescription ?? error.localizedDescription
